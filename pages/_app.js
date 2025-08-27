8c551e94792b14f78ed89ebc00dacaf6fac680fd
Renotrader31 <lagiog31@gmail.com>
2025-08-26 17:04:46 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import '../styles/global.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <script src="https://cdn.tailwindcss.com"></script>
+        <title>Quantum Trading Suite</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
